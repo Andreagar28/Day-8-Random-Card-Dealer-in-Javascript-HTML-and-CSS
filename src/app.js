1904632f38bd495db2ab0955e296ebaf55bde4fa
Andreagar28 <agarchig28@gmail.com>
2021-05-05 18:43:20 +0000

    card dealer

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  nuevaCarta();
+};
+const nuevaCarta = () => {
+  let cardContent = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    "Jack",
+    "Queen",
+    "King",
+    "Ace"
+  ];
+  let cardSuit = ["♥", "♠", "♣", "♦"];
+  let randomNumber = Math.floor(Math.random() * 14);
+  let randomSuit = Math.floor(Math.random() * 4);
+  // console.log(cardContent[randomNumber]);
+  // console.log(cardSuit[randomSuit]);
+
+  document.getElementById("cardDealer").innerHTML = cardContent[randomNumber];
+  document.getElementById("cardSign").innerHTML = cardSuit[randomSuit];
+  document.getElementById("cardSignBottom").innerHTML = cardSuit[randomSuit];
+
+  if (cardSuit[randomSuit] === "♥" || cardSuit[randomSuit] === "♦") {
+    var element = document.getElementById("cardSign");
+    element.classList.add("red");
+    var element2 = document.getElementById("cardSignBottom");
+    element2.classList.add("red");
+  } else if (cardSuit[randomSuit] === "♣" || cardSuit[randomSuit] === "♠") {
+    var element = document.getElementById("cardSign");
+    element.classList.remove("red");
+    var element2 = document.getElementById("cardSignBottom");
+    element2.classList.remove("red");
+  }
 };
